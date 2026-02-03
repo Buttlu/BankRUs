c4999819bba3c41553df26ffee1d81564efee0f7
Jacob <jackan202@gmail.com>
2026-02-03 13:08:51 +0100

    Add DTO's

---
+++ BankRUs.WebApi/Dtos/BankAccounts/AddBalanceDto.cs
@@ -0,0 +1,13 @@
+﻿using BankRUs.WebApi.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace BankRUs.WebApi.Dtos.BankAccounts;
+
+public record AddBalanceDto(
+    [MaxDecimals(2)]
+    [Required]
+    decimal Amount,
+    [MaxLength(140)]
+    [MinLength(1)]
+    string Reference
+);
---
+++ BankRUs.WebApi/Dtos/BankAccounts/AddBalanceResultDto.cs
@@ -0,0 +1,12 @@
+﻿namespace BankRUs.WebApi.Dtos.BankAccounts;
+
+public sealed record AddBalanceResultDto(
+    Guid TransactionId,
+    Guid UserId,
+    string Type,
+    decimal Amount,
+    string Currency,
+    string Reference,
+    DateTime CreatedAt,
+    decimal BalanceAfter
+);
