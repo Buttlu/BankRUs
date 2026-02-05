2854887e69430c7e1cac77a240e25b9d7989112b
Jacob <jackan202@gmail.com>
2026-02-05 11:36:35 +0100

    controller endpoint for getting a customer by id

--- BankRUs.WebApi/Controllers/CustomersController.cs
+++ BankRUs.WebApi/Controllers/CustomersController.cs
@@ -1,10 +1,13 @@
 ﻿using BankRUs.Application.UseCases.GetCustomers;
+using BankRUs.Domain.Entities;
 using BankRUs.Infrastructure.Authentication;
 using BankRUs.Infrastructure.Identity;
+using BankRUs.WebApi.Dtos.BankAccounts;
 using BankRUs.WebApi.Dtos.Customer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using System.Collections.Generic;
 
 namespace BankRUs.WebApi.Controllers;
 
@@ -28,7 +31,7 @@ public class CustomersController(
         if (pageSize < 1) pageSize = 1;
         else if (pageSize > _pageOptions.MaxPageSize) pageSize = _pageOptions.MaxPageSize;
 
-        var result = await _getCustomersHandler.HandleAsync(new GetCustomersQuery(
+        var result = await _getCustomersHandler.GetAllAsync(new GetCustomersQuery(
             Page: page,
             PageSize: pageSize
         ));
@@ -39,6 +42,36 @@ public class CustomersController(
             PageSize: result.PageMetaData.PageSize,
             TotalItems: result.PageMetaData.TotalCount,
             TotalPages: result.PageMetaData.TotalPages
+        );
+
+        return Ok(response);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        GetCustomerByIdResult result;
+        try {
+            result = await _getCustomersHandler.GetByIdAsync(new GetCustomerByIdQuery(
+                UserId: id
+            ));
+        } catch (ArgumentException) {
+            return NotFound();
+        }
+
+        var response = new GetCustomerByIdResponseDto(
+            Id: result.UserId,
+            FirstName: result.FirstName,
+            LastName: result.LastName,
+            Email: result.Email,
+            SocialSecurityNumber: result.SocialSecurityNumber,
+            BankAccounts: result.BankAccounts.Select(b => new BankAccountDto(
+                Id: b.Id,
+                Name: b.Name,
+                AccountNumber: b.AccountNumber,
+                Balance: b.Balance,
+                UserId: null
+            )).ToList()
         );
 
         return Ok(response);
