0bfe349ca923b051a3a919098b9b61e8d0de841a
Jacob <jackan202@gmail.com>
2026-02-05 11:36:08 +0100

    add extra query and result for getting a customer by id

---
+++ BankRUs.Application/UseCases/GetCustomers/GetCustomerByIdResult.cs
@@ -0,0 +1,12 @@
+﻿using BankRUs.Domain.Entities;
+
+namespace BankRUs.Application.UseCases.GetCustomers;
+
+public sealed record GetCustomerByIdResult(
+    Guid UserId,
+    string FirstName,
+    string LastName,
+    string Email,
+    string SocialSecurityNumber,
+    IReadOnlyList<BankAccount> BankAccounts
+);
--- BankRUs.Application/UseCases/GetCustomers/GetCustomersHandler.cs
+++ BankRUs.Application/UseCases/GetCustomers/GetCustomersHandler.cs
@@ -1,4 +1,6 @@
 ﻿using BankRUs.Application.Services;
+using BankRUs.Domain.Entities;
+using System.Collections.Generic;
 
 namespace BankRUs.Application.UseCases.GetCustomers;
 
@@ -6,13 +8,28 @@ public class GetCustomersHandler(ICustomerService customerService)
 {
     private readonly ICustomerService _customerService = customerService;
 
-    public async Task<GetCustomersResult> HandleAsync(GetCustomersQuery query)
+    public async Task<GetCustomersResult> GetAllAsync(GetCustomersQuery query)
     {
         var pagedCustomers = await _customerService.GetAllAsync(query);
 
         return new GetCustomersResult(
             Customers: pagedCustomers.Data,
             PageMetaData: pagedCustomers.MetaData
+        );
+    }
+
+    public async Task<GetCustomerByIdResult> GetByIdAsync(GetCustomerByIdQuery query)
+    {
+        var customer = await _customerService.GetByIdAsync(query.UserId)
+            ?? throw new ArgumentException("Customer not found");
+
+        return new GetCustomerByIdResult(
+            UserId: customer.CustomerId,
+            FirstName: customer.FirstName,
+            LastName: customer.LastName,
+            Email: customer.Email,
+            SocialSecurityNumber: customer.SocialSecurityNumber,
+            BankAccounts: customer.BankAccounts ?? []
         );
     }
 }
