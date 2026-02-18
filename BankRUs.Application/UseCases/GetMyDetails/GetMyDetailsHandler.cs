10b4db563ae01c883d6c3458edb483806684ea60
Jacob <jackan202@gmail.com>
2026-02-18 08:31:55 +0100

    updated my-details

--- BankRUs.Application/UseCases/GetMyDetails/GetMyDetailsHandler.cs
+++ BankRUs.Application/UseCases/GetMyDetails/GetMyDetailsHandler.cs
@@ -2,21 +2,27 @@
 
 namespace BankRUs.Application.UseCases.GetMyDetails;
 
-public class GetMyDetailsHandler(ICustomerService customerService)
+public class GetMyDetailsHandler(
+    ICustomerService customerService,
+    IBankAccountService bankAccountService
+)
 {
     private readonly ICustomerService _customerService = customerService;
+    private readonly IBankAccountService _bankAccountService = bankAccountService;
 
     public async Task<GetMyDetailsResult> HandleAsync(GetMyDetailsQuery query)
     {
         var customer = await _customerService.GetByIdAsync(query.UserId)
             ?? throw new ArgumentException("Customer not found");
 
+        var bankAccounts = await _bankAccountService.GetByUserId(query.UserId);
+
         return new GetMyDetailsResult(
             Id: customer.CustomerId,
             Email: customer.Email,
             FullName: $"{customer.FirstName} {customer.LastName}",
             SocialSecurityNumber: customer.SocialSecurityNumber,
-            BankAccounts: customer.BankAccounts ?? []
+            BankAccounts: bankAccounts ?? []
         );
     }
 }
