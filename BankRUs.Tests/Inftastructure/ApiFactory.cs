62057461a8b9b3c0bfd56420937b0ee0d4eb1561
Jacob <jackan202@gmail.com>
2026-02-20 11:43:25 +0100

    factory to use SQLite instead of SQL Server during tests

---
+++ BankRUs.Tests/Inftastructure/ApiFactory.cs
@@ -0,0 +1,56 @@
+﻿using BankRUs.Infrastructure.Persistance;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+
+namespace BankRUs.Tests.Inftastructure;
+
+public class ApiFactory : WebApplicationFactory<Program>
+{
+    private SqliteConnection? _connection;
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            // Remove previous DbContext
+            services.RemoveAll<ApplicationDbContext>();
+            services.RemoveAll<DbContextOptions<ApplicationDbContext>>();
+            services.RemoveAll<IConfigureOptions<DbContextOptions<ApplicationDbContext>>>();
+            services.RemoveAll<IOptionsChangeTokenSource<DbContextOptions<ApplicationDbContext>>>();
+
+            var toRemove = services
+                .Where(s => s.ServiceType.FullName is not null &&
+                            s.ServiceType.FullName.Contains("IDbContextOptionsConfiguration") &&
+                            s.ServiceType.GenericTypeArguments.Contains(typeof(ApplicationDbContext)))
+                .ToList();
+            foreach (var s in toRemove)
+                services.Remove(s);
+
+            // Add SQLite
+            _connection = new SqliteConnection("DataSource=:memory:");
+            _connection.Open();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlite(_connection);
+            });
+
+            // Skapa Db
+            using var sp = services.BuildServiceProvider();
+            using var scope = sp.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            db.Database.EnsureCreated();
+        });
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+        _connection?.Dispose();
+    }
+}
